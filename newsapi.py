bd43f1c846c20ccfaca3aca042fcd58bf70846ac
KMicha <michael.kahle@yahoo.de>
2026-01-12 13:38:18 +0100

    Update newsapi.py

--- newsapi.py
+++ newsapi.py
@@ -583,7 +583,7 @@ def inqRandomNews():
         url = ('https://newsapi.org/v2/everything?'+
             #'q="'+keyWord+'"&'
             'q='+keyWord+'&'
-            'to=2025-12-25&'
+            'to=2026-01-05&'
             'pageSize='+str(pageSize)+'&'
             'language='+language+'&'
             'page='+str(currPage)+'&'
